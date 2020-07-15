5560e89cfcaf61c80db57b9eba01b3f83323cc95
boyyang-love <boyyang-love@outlook.com>
2020-07-15 11:00:37 +0800

    vue3.0学习第一次提交

---
+++ src/router/index.js
@@ -0,0 +1,26 @@
+import {
+  createRouter,
+  createWebHashHistory
+} from "vue-router";
+
+// import Home from '@/views/Home.vue'
+const routes = [
+
+  {
+    path: '/',
+    name: 'Home',
+    component: () => import ('@/views/Home.vue')
+  },
+  // {
+  //   path: '/',
+  //   name: 'Home',
+  //   component: Home
+  // }
+];
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes
+});
+
+export default router;
---
+++ src/views/Home.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="home">
+    <h1>this is home</h1>
+    <h2>{{mes}}</h2>
+    <div v-for="(item, i) in state" :key="i">{{item}}</div>
+    <button @click="add">add</button>
+  </div>
+</template>
+
+<script>
+import { ref, reactive } from "vue";
+export default {
+  name: "Home",
+  setup() {
+    let mes = ref(100);
+    const state = reactive({
+      name: "boyyang",
+      sex: "boy"
+    });
+
+    const add = () => {
+      mes.value++;
+      state.name = 'i love you'
+    };
+
+    return {
+      mes,
+      state,
+      add
+    };
+  }
+
+};
+</script>
+
+<style scoped>
+</style>
