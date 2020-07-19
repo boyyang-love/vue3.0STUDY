d8e2e80bf1918a5fcb6bbaf62bf1581f30721aec
boyyang-love <boyyang-love@outlook.com>
2020-07-19 21:32:44 +0800

    2222

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.a86af92f09577bb28c6e7ca92b0e0c68.js"
+  "/precache-manifest.096d6e05dc595f046ce03f2fd06294e4.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "vue-next"});
